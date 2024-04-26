d0f3b087155a612554c01abc7b6adb0b1a178d0e
guilhermejr92 <guizao_167@hotmail.com>
2024-04-25 21:30:22 -0300

    novo comando atualizado

--- index.js
+++ index.js
@@ -1,27 +1,41 @@
-// Variáveis para armazenar o nome e a quantidade de experiência (XP) do herói
-let nomeHeroi = "Paladin";
-let xpHeroi = Math.floor(Math.random() * 20000); // Exemplo de XP do herói aletarória
+const readline = require('readline');
 
-// Utilizando uma estrutura de decisão para determinar o nível do herói com base na XP
-let nivelHeroi;
-
-if (xpHeroi < 1000) {
-    nivelHeroi = "Ferro";
-} else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
-    nivelHeroi = "Bronze";
-} else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
-    nivelHeroi = "Prata";
-} else if (xpHeroi >= 5001 && xpHeroi <= 7000) {
-    nivelHeroi = "Ouro";
-} else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
-    nivelHeroi = "Platina";
-} else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
-    nivelHeroi = "Ascendente";
-} else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
-    nivelHeroi = "Imortal";
-} else {
-    nivelHeroi = "Radiante";
+// Função para calcular o nível do herói com base na XP
+function calcularNivelHeroi(xp) {
+    if (xp < 1000) {
+        return "Ferro";
+    } else if (xp >= 1001 && xp <= 2000) {
+        return "Bronze";
+    } else if (xp >= 2001 && xp <= 5000) {
+        return "Prata";
+    } else if (xp >= 5001 && xp <= 7000) {
+        return "Ouro";
+    } else if (xp >= 7001 && xp <= 8000) {
+        return "Platina";
+    } else if (xp >= 8001 && xp <= 9000) {
+        return "Ascendente";
+    } else if (xp >= 9001 && xp <= 10000) {
+        return "Imortal";
+    } else {
+        return "Radiante";
+    }
 }
 
-// Exibindo a mensagem com o nome e o nível do herói
-console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivelHeroi}`);
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// Solicitar nome do herói ao usuário
+rl.question("Digite o nome do herói: ", function (nomeHeroi) {
+    // Gerar XP aleatória entre 0 e 20000
+    let xpHeroi = Math.floor(Math.random() * 20000);
+
+    // Calcular o nível do herói com base na XP
+    let nivelHeroi = calcularNivelHeroi(xpHeroi);
+
+    // Exibindo a mensagem com o nome e o nível do herói
+    console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivelHeroi} com XP ${xpHeroi}`);
+
+    rl.close();
+});
